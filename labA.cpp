d36562e69d995d0805038bd3f02a63cf12e04179
LieneLittlebird <liene.putnina20@gmail.com>
2021-03-01 12:48:18 +0200

    input validation added

--- labA.cpp
+++ labA.cpp
@@ -5,6 +5,7 @@
 //TEST CASES
 
 #include <iostream> //Wrote stuff here
+#include <limits>
 using namespace std;
 
 //A function containing the instructions for the program
@@ -16,18 +17,63 @@ int findNumbers()
     int count;
     int i;
     int minValue;
-
-    // check if right with every cin
+    bool continueLoop = true;
 
     // UI for the program
     cout << "Enter the number of elements in an array\n";
-    cin >> count; //  Check if good
+
+    // User enters the volume of the array by writing a positive integer
+    cin >> count;
+
+    while (continueLoop)
+    {
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input value. Please, enter an integer" << endl;
+            cin >> count;
+        }
+        if (!cin.fail())
+            break;
+    }
+
+    // int inputValue
+    // bool continuLoop
+
+    // while (continuLoop) {
+    // cout >> enter a int value
+    // cin >> something
+    // if (somethin is int) {
+    // inputValue = something
+    // continueLoop = false
+    // }
+    // else {
+    // cout >> wrong format, please input int
+    // }
+    // }
+
+    // Program prints the enter message with the number of the array volume the user has entered
     cout << "Enter " << count << " numbers \n";
 
     // A loop that goes through the array of numbers the user has entered and prints them all out
     for (i = 0; i < count; i++)
     {
-        cin >> numbers[i]; // Save entered value to temp var. Check if entered value is a number and then print out
+        cin >> numbers[i];
+        // A loop that checks if the wright data type is entered,
+        // clears the error log and lets the user try again with different data types
+        while (continueLoop)
+        {
+            if (cin.fail())
+            {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid input value. Please, enter an integer" << endl;
+                cin >> count;
+            }
+            if (!cin.fail())
+                break;
+        }
     }
 
     // A loop that:
@@ -37,8 +83,10 @@ int findNumbers()
     int result = 0;
     for (i = 0; i < count; i++)
     {
+        // Checking if minValue is bigger than every element in the numbers array
         if (numbers[i] < minValue)
         {
+            // Comparing every value of numbers array to minValue
             minValue = numbers[i];
         }
         for (i = 0; i < count; i++)
@@ -51,7 +99,7 @@ int findNumbers()
     }
 
     cout << "The smallest number is: " << minValue << "."
-         << " It occurs " << result << " times" << endl;
+         << " It occurs " << result << " times. " << endl;
 
     return 0;
 }
@@ -65,7 +113,7 @@ int main()
     do
     {
         findNumbers();
-        cout << "Enter 1 to continue or 0 to quit: " << endl;
+        cout << " Enter 1 to continue or 0 to quit: " << endl;
         cin >> repeatExec;
     } while (repeatExec == 1);
 }
